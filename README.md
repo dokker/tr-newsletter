81f5ce2446a1325356350c80948dabea5cb4a5f7
docker <dock3r@gmail.com>
2016-03-18 15:22:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Transparency Newsletter Generator
+
+Generates rss feed that can be used by mailchimp RSS template.
+
+Needs the following CPTs:
+- News
+- Event
+- Newsletter
---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Transparency Newsletter Generator
+Plugin URI: http://colorandcode.hu
+Description: Generates rss feed for Mailchimp templates
+Version: 1.0
+Author: docker
+Author URI: https://hu.linkedin.com/in/docker
+*/
