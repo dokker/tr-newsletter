3a54d845553d366c885b6bc27a7b57be31458c13
docker <dock3r@gmail.com>
2016-03-24 14:37:59 +0100

    Fire plugin functionality

--- init.php
+++ init.php
@@ -1,9 +1,34 @@
 <?php
 /*
 Plugin Name: Transparency Newsletter Generator
-Plugin URI: http://colorandcode.hu
+Plugin URI: https://github.com/dokker/tr-newsletter
 Description: Generates rss feed for Mailchimp templates
 Version: 1.0
 Author: docker
 Author URI: https://hu.linkedin.com/in/docker
 */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Autoloading classes
+spl_autoload_register(function ($class) {
+	$segments = array_filter(explode("\\", $class));
+
+	if (array_shift($segments) === "TRNewsletter") {
+		$path = __DIR__ . "/" . implode("/", $segments) . ".php";
+		if (file_exists($path)) {
+			include $path;
+		}
+	}
+});
+
+
+add_action('init', function(){
+	$newsletter = new TRNewsletter\Newsletter();
+	$newsletter->registerFeed('tr-news');
+	$newsletter->registerFeed('tr-events');
+	$newsletter->registerFeed('tr-lead');
+});
